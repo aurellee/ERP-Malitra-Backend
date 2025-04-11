7e003737ac5e0e233a3e7c761c9162a31ecadff8
Jaqueline Aurelia L. <aurelialangi06@gmail.com>
2025-04-11 11:18:01 +0700

    feat: add url product

--- erp_malitra_service/urls.py
+++ erp_malitra_service/urls.py
@@ -16,15 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-# from malitra_service.views.user_views import CreateUserView, CustomTokenObtainPairView
-# from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from malitra_service.views.user_views import CreateUserView, CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("register/", CreateUserView.as_view(), name="register"),
-    # path("token/", CustomTokenObtainPairView.as_view(), name="get_token"),
-    # path("refresh/", TokenRefreshView.as_view(), name="refresh"),
-    # path("", include("malitra_service.urls.product_urls")),
-    # path("", include("malitra_service.urls.ekspedisi_masuk_urls")),
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", CustomTokenObtainPairView.as_view(), name="get_token"),
+    path("refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("", include("malitra_service.urls.product_urls")),
+    path("", include("malitra_service.urls.ekspedisi_masuk_urls")),
 ]
-
