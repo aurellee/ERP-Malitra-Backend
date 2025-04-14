ae35a9054c82d99c387c4c4eead8d08d05ad429e
Jaqueline Aurelia L. <aurelialangi06@gmail.com>
2025-04-14 23:21:19 +0700

    modify model ekspedisi masuk

--- malitra_service/models.py
+++ malitra_service/models.py
@@ -71,7 +71,7 @@ class Employee(models.Model):
 
 class EkspedisiMasuk(models.Model):
     ekspedisi_id = models.AutoField(primary_key=True)
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     date = models.DateTimeField(auto_now_add=True)
     quantity = models.IntegerField()
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
