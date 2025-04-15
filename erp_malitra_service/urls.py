5b2657a6c75c35725a9c94af0a685d362819c467
Jaqueline Aurelia L. <aurelialangi06@gmail.com>
2025-04-15 17:35:07 +0700

    fix: modify urls and updates employee_serializers

--- erp_malitra_service/urls.py
+++ erp_malitra_service/urls.py
@@ -20,10 +20,16 @@ from malitra_service.views.user_views import CreateUserView, CustomTokenObtainPa
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    # Admin & Auth
     path('admin/', admin.site.urls),
     path("register/", CreateUserView.as_view(), name="register"),
     path("token/", CustomTokenObtainPairView.as_view(), name="get_token"),
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
-    path("", include("malitra_service.urls.product_urls")),
+    
+    # Include modular URLs
+    path("", include("malitra_service.urls.dashboard_urls")),
     path("", include("malitra_service.urls.ekspedisi_masuk_urls")),
+    path("", include("malitra_service.urls.employee_urls")),
+    path("", include("malitra_service.urls.invoice_urls")),
+    path("", include("malitra_service.urls.product_urls")),
 ]
